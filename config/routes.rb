3a8c5cc656e6750f2db3f920f88c0d85584a4ab1
Elodie Ravaton <elodiemegane012@gmail.com>
2023-05-25 16:42:39 +0400

    router updated

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :lists
+  root to: 'lists#index'
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: :destroy
+
   # get 'lists', to: 'lists#index'
   # get 'lists/new', to: 'lists#new', as: :new_list
   # post 'lists', to: 'lists#create'
   # get 'lists/:id', to: 'lists#show', as: :list
-
 end
